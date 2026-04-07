da844dfba351d27e13fb199e873ea7e346771f19
Usama El-Sharkawi <usamasharkawi@gmail.com>
2026-04-07 20:03:16 +0200

    Managing the Token Expiration.

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,9 +1,28 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import { Outlet, useNavigation, useSubmit } from "react-router-dom";
 
-import MainNavigation from '../components/MainNavigation';
+import MainNavigation from "../components/MainNavigation";
+import { getAuthToken, getTokenDuration } from "../util/auth";
+import { useEffect } from "react";
 
 function RootLayout() {
-  // const navigation = useNavigation();
+  const token = getAuthToken();
+  const submit = useSubmit();
+
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+    if (token === "EXPIRED") {
+      submit(null, { action: "/logout", method: "post" });
+      return;
+    }
+
+    const tokenDuration = getTokenDuration();
+    console.log(tokenDuration);
+    setTimeout(() => {
+      submit(null, { action: "/logout", method: "post" });
+    }, tokenDuration);
+  }, [token, submit]);
 
   return (
     <>
--- frontend/src/util/auth.js
+++ frontend/src/util/auth.js
@@ -1,19 +1,38 @@
 import { redirect } from "react-router-dom";
 
-export function getAuthToken(){
-    const token = localStorage.getItem('token');
-    return token;
+export function getTokenDuration() {
+  const storedExpirationDate = localStorage.getItem("expiration");
+  const expirationDate = new Date(storedExpirationDate);
+  const now = new Date();
+  const tokenDuration = expirationDate.getTime() - now.getTime();
+  return tokenDuration;
 }
 
-export function tokenLoader(){
-    const token = getAuthToken();
-    return token;
-}
+export function getAuthToken() {
+  const token = localStorage.getItem("token");
 
-export function checkAuthLoader(){
-    const token = getAuthToken();
-    if(!token){
-        return redirect('/auth');
-    }
+  if (!token) {
     return null;
+  }
+
+  const tokenDuration = getTokenDuration();
+
+  if (tokenDuration < 0) {
+    return "EXPIRED";
+  }
+
+  return token;
+}
+
+export function tokenLoader() {
+  const token = getAuthToken();
+  return token;
+}
+
+export function checkAuthLoader() {
+  const token = getAuthToken();
+  if (!token) {
+    return redirect("/auth");
+  }
+  return null;
 }
